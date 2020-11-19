a5aaa793e12c67ba05d14259a6ad2479aae20e45
s v <chofexx@gmail.com>
2020-11-19 16:08:49 +0200

    Ticket: Delete a Comment DONE

--- MongoUniversity/src/CourseProject/M220N/Repositories/CommentsRepository.cs
+++ MongoUniversity/src/CourseProject/M220N/Repositories/CommentsRepository.cs
@@ -103,7 +103,7 @@ namespace M220N.Repositories
             _commentsCollection.DeleteOne(
                 Builders<Comment>.Filter.Where(
                     c => c.MovieId == movieId
-                         && c.Id == commentId));
+                         && c.Id == commentId && c.Email == user.Email));
 
             return await _moviesRepository.GetMovieAsync(movieId.ToString(), cancellationToken);
         }
